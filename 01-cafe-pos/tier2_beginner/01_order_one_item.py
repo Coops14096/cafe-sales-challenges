1da893e20d6628bca181e324393c73999c3773ad
Cooper <COOPER.HENRICKS@MY.MRC.TAS.EDU.AU>
2026-08-03 12:31:40 +1000

    Update 01_order_one_item.py

--- 01-cafe-pos/tier2_beginner/01_order_one_item.py
+++ 01-cafe-pos/tier2_beginner/01_order_one_item.py
@@ -40,7 +40,12 @@ MENU = {
     "Toastie": 6.50,
     "Hot Chocolate": 4.00,
 }
-
+order_item = input("What would you like?")
+if order_item in MENU:
+      print(f"Good Choice! {order_item} will cost {MENU[order_item]}!")
+else:
+   print(f"Sorry we do not currently have {order_item} in stock)
+   
 # TODO 1: ask the customer what they would like, using input(), and store
 #         their answer in a variable called order_item
 
