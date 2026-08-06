5c2b81c5d3b472a06ee6d1473665a32ed405b015
Cooper <COOPER.HENRICKS@MY.MRC.TAS.EDU.AU>
2026-08-06 03:47:34 +0000

    tested and finalised

--- 01-cafe-pos/tier2_beginner/01_order_one_item.py
+++ 01-cafe-pos/tier2_beginner/01_order_one_item.py
@@ -40,9 +40,9 @@ MENU = {
     "Toastie": 6.50,
     "Hot Chocolate": 4.00,
 }
-order_item = input("What would you like?")
+order_item = input("What would you like? ")
 if order_item in MENU:
-   print(f"Good Choice! {order_item} will cost {MENU[order_item]}!")
+   print(f"Good Choice! {order_item} will cost ${MENU[order_item]}!")
 else:
    print(f"Sorry we do not currently have {order_item} in stock")
    
