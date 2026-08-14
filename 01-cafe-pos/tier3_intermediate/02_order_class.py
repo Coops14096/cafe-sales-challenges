d2ab66c61f5bd80292aee2b1a7bea3820fddec23
Cooper <COOPER.HENRICKS@MY.MRC.TAS.EDU.AU>
2026-08-14 04:07:36 +0000

    hi

--- 01-cafe-pos/tier3_intermediate/02_order_class.py
+++ 01-cafe-pos/tier3_intermediate/02_order_class.py
@@ -37,38 +37,36 @@ class Order:
     """Represents one customer's order at The Trendiest Café."""
 
     def __init__(self):
-        """Set up an empty order. Hint: you'll need a list to store item names."""
-        # TODO: create self.items as an empty list
-        pass
+        self.items = []
+
 
     def add_item(self, item_name):
-        """
-        Add item_name to this order if it exists on the MENU.
-        Print "Added {item_name} to the order." if successful.
-        Print "Sorry, {item_name} is not on the menu." if it isn't a valid item.
-        """
-        # TODO: implement this method
-        pass
+        if item_name in MENU:
+            self.items.append(item_name)
+            print(f"Added {item_name} to your order!")
+        else:
+            print("Not in this store bucko")
 
     def remove_item(self, item_name):
-        """
-        Remove ONE occurrence of item_name from this order, if present.
-        Print "Removed {item_name} from the order." if successful.
-        Print "{item_name} isn't in this order." if it wasn't there.
-        """
-        # TODO: implement this method
-        pass
+        if item_name in self.items:
+            self.items.remove(item_name)
+            print(f"Removed {item_name} from your order!")
+        else:
+            print(f"You have not ordered {item_name}")
 
     def get_total(self):
-        """Return the total price of everything currently in self.items."""
-        # TODO: implement this method
-        pass
+        totalprice = 0
+        for item_name in self.items:
+            totalprice += MENU[item_name]
+        return totalprice
+
 
 
 if __name__ == "__main__":
     my_order = Order()
     my_order.add_item("Coffee")
     my_order.add_item("Muffin")
+    my_order.add_item("Toastie")
     my_order.add_item("Tea")
     my_order.remove_item("Tea")
 
